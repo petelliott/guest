187f88d449a3a0b3a12084e62e33ae974c14ddd6
Peter Elliott <pelliott@ualberta.ca>
2019-09-06 16:17:30 -0600

    maybe fixed install script

--- install.sh
+++ install.sh
@@ -2,13 +2,12 @@
 
 # install guest for travis
 
-
 # install dependecies
 sudo apt update
 sudo apt install -y guile-2.2 lcov
 gem install coveralls-lcov
 
-SITEDIR=guile -c "(display (%site-dir))(newline)"
+SITEDIR=$(guile -c "(display (%site-dir))(newline)")
 
 # install guest
 sudo git clone --depth 1 https://github.com/Petelliott/guest.git $SITEDIR
