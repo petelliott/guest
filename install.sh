bcf9601a9f2dc6a2b57051666c1038222c7f503a
Peter Elliott <pelliott@ualberta.ca>
2019-09-06 19:38:46 -0600

    fixed git clone

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ gem install coveralls-lcov
 SITEDIR=$(guile -c "(display (%site-dir))(newline)")
 
 # install guest
-sudo git clone --depth 1 https://github.com/Petelliott/guest.git $SITEDIR
+sudo git clone --depth 1 https://github.com/Petelliott/guest.git $SITEDIR/guest
 sudo mv $SITEDIR/guest/guest /usr/bin/
